8435b0c85933882913467804a4c7b0dfa9235d82
dwinarwastu <admin@dwinarwastu.com>
2024-09-30 16:57:43 +0700

    login & register api

--- app/Http/Controllers/AuthLoginController.php
+++ app/Http/Controllers/AuthLoginController.php
@@ -22,39 +22,54 @@ class AuthLoginController extends Controller
      */
     public function create(Request $request)
     {
-        $validatedData = $request->validate([
-            'nama' => 'required|string|max:255',
-            'email' => 'required|unique:pengguna',
-            'alamat' => 'required|string|max:255',
-            'no_telp' => 'required||max:14|unique:pengguna',
-            'username' => 'required|string|unique:pengguna',
-            'password' => 'required|string|min:8',
-        ], [
-            'nama.required' => 'Nama harus diisi',
-            'email.required' => 'Email harus diisi',
-            'alamat.required' => 'Alamat bekerja harus diisi',
-            'no_tlp.required' => 'No_tlp harus diisi',
-            'username.required' => 'Username harus diisi',
-            'password.min' => 'Password minimal 8',
-        ]);
+        // $validatedData = $request->validate([
+        //     'nama' => 'required|string|max:255',
+        //     'email' => 'required|unique:pengguna',
+        //     'alamat' => 'required|string|max:255',
+        //     'no_telp' => 'required||max:14|unique:pengguna',
+        //     'username' => 'required|string|unique:pengguna',
+        //     'password' => 'required|string|min:8',
+        // ], [
+        //     'nama.required' => 'Nama harus diisi',
+        //     'email.required' => 'Email harus diisi',
+        //     'alamat.required' => 'Alamat bekerja harus diisi',
+        //     'no_tlp.required' => 'No_tlp harus diisi',
+        //     'username.required' => 'Username harus diisi',
+        //     'password.min' => 'Password minimal 8',
+        // ]);
 
-        // Hash password
-        $validatedData['password'] = bcrypt($validatedData['password']);
+        // // Hash password
+        // $validatedData['password'] = bcrypt($validatedData['password']);
 
-        $user = new User();
-        $user->nama = $validatedData['nama'];
-        $user->email = $validatedData['email'];
-        $user->alamat = $validatedData['alamat'];
-        $user->no_telp = $validatedData['no_telp'];
-        $user->username = $validatedData['username'];
-        $user->password = $validatedData['password'];
-        $succ = $user->save();
+        // $user = new User();
+        // $user->nama = $validatedData['nama'];
+        // $user->email = $validatedData['email'];
+        // $user->alamat = $validatedData['alamat'];
+        // $user->no_telp = $validatedData['no_telp'];
+        // $user->username = $validatedData['username'];
+        // $user->password = $validatedData['password'];
+        // $succ = $user->save();
 
-        // Redirect kembali ke halaman kelola karyawan dengan pesan sukses
-        if ($succ){
+        try {
+            $client = new \GuzzleHttp\Client();
+
+            $response = $client->request('POST', 'http://127.0.0.1:8000/api/register', [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                ],
+                'body' => json_encode([
+                    'nama' => $request->nama,
+                    'no_telp' => $request->no_telp,
+                    'email' => $request->email,
+                    'username' => $request->username,
+                    'password' => $request->password,
+                    'alamat' => $request->alamat,
+                ])
+            ]);
+
             return redirect('/login')->with('success', 'Berhasil Melakukan pendaftaran');
-        } else {
-            return back();
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return back()->withInput();
         }
     }
 
@@ -63,17 +78,33 @@ class AuthLoginController extends Controller
      */
     public function auth(Request $request)
     {
-        $credential = $request->validate([
-            'username'=>'required',
-            'password'=>'required'
-        ]);
+        // $credential = $request->validate([
+        //     'username'=>'required',
+        //     'password'=>'required'
+        // ]);
 
-        if(Auth::Attempt($credential)){
-            $request->session()->regenerate();
-            return redirect("/");
+        // if(Auth::Attempt($credential)){
+        //     $request->session()->regenerate();
+        //     return redirect("/");
+        // }
+
+        try {
+            $client = new \GuzzleHttp\Client();
+
+            $response = $client->request('POST', 'http://127.0.0.1:8000/api/login', [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                ],
+                'body' => json_encode([
+                    'username' => $request->username,
+                    'password' => $request->password,
+                ])
+            ]);
+
+            return redirect('/');
+        } catch (\GuzzleHttp\Exception\ClientException) {
+            return back()->with('loginError', 'Login Gagal');
         }
-
-        return back()->with('loginError', 'Login Gagal');
     }
 
     /**
