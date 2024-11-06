7b5c38f21760adc160062fe730728cb80e2d1a52
delia041015 <delialistiana025@gmail.com>
2024-11-06 19:21:53 +0700

    nav

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,41 +1,51 @@
-<div class="sidebar col-sm-4 col-md-6 col-lg-8">
-    <div class="text-center">
-        <img src="{{ asset('images/logoevmo.png') }}" alt="Logo" class="img-fluid" style="width: 150px;">
-    </div>
-    {{-- @auth
-        @if (auth()->user()->role == 1)
-            sidebar untuk penyedia jasa
-        @else
-            customer biasa aja ini
-        @endif --}}
-        <div class="d-flex flex-column align-items-center">
-            <div class="p-2">
-                <a href="/"><img src="{{ asset('icon/bxs_home.png') }}" alt="home" class="img-fluid" style="width: 30px;"></a>
-            </div>
-            {{-- @if (auth()->user()->role == 1) --}}
-            {{-- @if($role==1) --}}
-                <div class="p-2">
-                    <a href="/datapesanan"><img src="{{ asset('icon/icon-park-solid_transaction.png') }}" alt="Detail Pesanan" class="img-fluid" style="width: 30px;"></a>
-                </div>
-            {{-- @else --}}
-            <div class="p-2">
-                <a href="/status_pemesanan"><img src="{{ asset('icon/icon-park-solid_transaction.png') }}" alt="Detail Pesanan" class="img-fluid" style="width: 30px;"></a>
-            </div>
-            {{-- @endif --}}
-            {{-- <div class="p-2">
-                <a href="/notifikasi"><img src="{{ asset('icon/iconamoon_notification-fill.png') }}" alt="Notification" class="img-fluid" style="width: 30px;"></a>
-            </div> --}}
-            <div class="p-2">
-                <a href="/profilcust"><img src="{{ asset('icon/iconamoon_profile-fill.png') }}" alt="Profil" class="img-fluid" style="width: 30px;"></a>
-            </div>
-            {{-- <div class="p-2">
-                <a href="https://wa.me"><img src="{{ asset('icon/ri_whatsapp-fill.png') }}" alt="WhatsApp" class="img-fluid" style="width: 30px;"></a>
-            </div>
-            <div class="p-2">
-                <a href="/helpcenter"><img src="{{ asset('icon/ic_baseline-help.png') }}" alt="Help Center" class="img-fluid" style="width: 30px;"></a>
-            </div> --}}
+<head>
+    <style>
+        .custom-bg {
+    background-color: #f8f9fa !important;
+}
+
+.navbar-light .navbar-nav .nav-link,
+.navbar-light .navbar-text {
+    color: #343a40;
+}
+
+.navbar-light .navbar-nav .nav-link:hover {
+    color: #007bff;
+}
+
+.small-padding .nav-link {
+    padding: 5px 10px !important; /* Mengatur padding lebih kecil */
+}
+
+.navbar-brand {
+    margin-right: 15px; /* Mengurangi spasi antara logo dan item navbar */
+}
+
+    </style>
+</head>
+<header class="navbar navbar-expand-lg navbar-light custom-bg">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('images/logoevmo.png') }}" alt="Logo" class="img-fluid" style="width: 80px;">
+        </a>
+
+        @auth
+        <div class="navbar-nav ms-auto d-flex flex-row align-items-center small-padding">
+            <a class="nav-link" href="/">Home</a>
+
+            @if (auth()->user()->role == 1)
+                {{-- Navbar link for service providers --}}
+                <a class="nav-link" href="/datapesanan">Pesanan</a>
+            @else
+                {{-- Navbar link for regular customers --}}
+                <a class="nav-link" href="/status_pemesanan">Pesanan</a>
+            @endif
+
+            <a class="nav-link" href="/profilcust">Profil</a>
         </div>
-    {{-- @else
-        KOSONG AJA YA
-    @endauth --}}
-</div>
+        @else
+            {{-- Display this if user is not authenticated --}}
+            <span class="navbar-text ms-auto">Silakan login untuk melihat menu.</span>
+        @endauth
+    </div>
+</header>
