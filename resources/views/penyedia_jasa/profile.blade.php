56cf83365abd2d2b771f0609af0c3c6f58f7288b
SirRHO <rizkinurrohman4424@gmail.com>
2024-11-14 10:01:11 +0700

    identitas

--- resources/views/penyedia_jasa/profile.blade.php
+++ resources/views/penyedia_jasa/profile.blade.php
@@ -11,25 +11,34 @@
         background-color: #f9f9f9;
     }
     .account-header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 20px;
+    text-align: left; /* Agar gambar dan teks berada di tengah */
+}
+
+.account-header h3 {
+    margin-bottom: 10px; /* Jarak antara judul dan gambar */
+}
+
+.account-header img {
+    display: block; /* Mengatur gambar menjadi block untuk kontrol posisi yang lebih baik */
+    margin: 0 auto; /* Agar gambar tetap di tengah */
+    width: 120px; /* Sesuaikan ukuran */
+        height: 120px; /* Pastikan tinggi dan lebar sama untuk bentuk lingkaran */
+        border-radius: 50%; /* Membuat gambar menjadi lingkaran */
+        object-fit: cover; /* Memastikan gambar memenuhi lingkaran tanpa terdistorsi */
+        border: 2px solid #365B80;  /* Agar gambar berbentuk lingkaran */
+}
+    .account-details{
+        margin-top: 30px;
     }
-    .account-header h2 {
-        margin: 0;
-    }
-    .account-header img {
-        width: 80px;
-        height: 80px;
-        border-radius: 50%;
-    }
+
     .account-details p {
         margin: 10px 0;
+        font-size: 20px;
     }
     .account-details strong {
         display: inline-block;
         width: 150px;
+        font-size: 20px;
     }
 </style>
 <div class="account-container">
@@ -38,12 +47,13 @@
         <img src="{{ asset('images/avatar/3.jpg') }}" alt="User Profile">
     </div>
     <div class="account-details">
-        <p><strong>Nama:</strong> {{ $user['nama'] }}</p>
-        <p><strong>Username:</strong> {{ $user['username'] }}</p>
-        <p><strong>Email:</strong> {{ $user['email'] }}</p>
-        <p><strong>No. Telp:</strong> {{ $user['no_telp'] }}</p>
-        <p><strong>Alamat:</strong> {{ $user['alamat'] }}</p>
-        <p><strong>Role:</strong> {{ $user['role'] == 0 ? 'User' : 'Admin' }}</p>
+        <p><strong>Nama</strong>        : {{ $user['nama'] }}</p>
+        <p><strong>Username</strong>    : {{ $user['username'] }}</p>
+        <p><strong>Email</strong>       : {{ $user['email'] }}</p>
+        <p><strong>No. Telp</strong>    : {{ $user['no_telp'] }}</p>
+        <p><strong>Alamat</strong>      : {{ $user['alamat'] }}</p>
+        <p><strong>Role</strong>        : {{ $user['role'] == 0 ? 'User' : 'Admin' }}</p>
+        <button type="button" class="btn btn-primary" style="margin-top: 10px;">Simpan</button>
     </div>
 </div>
 @endsection
